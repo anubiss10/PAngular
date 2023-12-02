cc483b0639b8d1329fb86620120f1567caaae76b
Julian Arce <julianfelipearcecalvo@gmail.com>
2023-12-02 17:27:39 +0100

    practica event y property binding

--- app/src/app/empleado/empleado.component.ts
+++ app/src/app/empleado/empleado.component.ts
@@ -2,15 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-templateUrl: './empleado.component.html',
- //template:"<p>unidad de empleado</p>",
-styleUrls: ['./empleado.component.css']
+  templateUrl: './empleado.component.html',
+  //template:"<p>unidad de empleado</p>",
+  styleUrls: ['./empleado.component.css']
   //styles:["p{background-color:red}"]
 })
 export class EmpleadoComponent {
-  nombre="Carlos";
-  apellido="erina";
-   edad=15;
-  empresa="cesur games"
+  nombre = "Carlos";
+  apellido = "erina";
+  edad = 15;
+  empresa = "cesur games"
+  // getEdad() {
+  //   return this.edad;
+  // }
+  // llamaEmpresa(value:String){
+
+  // }
+  habilitacionCuadro = true;
+  usuRegistrado = false;
+
+  getRegistro() {
+    this.usuRegistrado = false;
+  }
+
+  setusuarioRegistrado(event: Event) {
+    // alert("El usuario se acaba de registrar")
+    // this.textodeRegistro = "El usuario se acaba de registrar"
+    if((<HTMLInputElement>event.target).value=="si"){
+      this.textodeRegistro="el usuario se acaba de registrar"
+    }
+    else{
+      this.textodeRegistro="el usuario no se acaba de registrar"
+    }
+  }
+  textodeRegistro = "no hay nadie registrado";
 
 }
